fca14bb2b6142b7d952ae2bce809312d30614a67
codeyourwaytofreedom <can.kolej@gmail.com>
2022-12-28 17:10:58 +0300

    First test with single flying parrot.
    Not so smooth but great step for the first trial.
    
    Great start to the new project.
    
    Keep going.
    
    Remember why you started.

---
+++ src/forest/forest.js
@@ -0,0 +1,28 @@
+import "./forest.css";
+import parrot1 from "./images/parrot1.png";
+import parrot2 from "./images/parrot2.png";
+import parrot3 from "./images/parrot3.png";
+import parrot4 from "./images/parrot4.png";
+import parrot5 from "./images/parrot5.png";
+import parrot6 from "./images/parrot6.png";
+import parrot7 from "./images/parrot7.png";
+import parrot8 from "./images/parrot8.png";
+
+const Forest = () => {
+    return ( 
+    
+    <div className="forest">
+        <div className="forest_test">
+            
+        </div>
+        <img src={parrot1} alt="" />
+        <img src={parrot2} alt="" />
+        <img src={parrot3} alt="" />
+        <img src={parrot4} alt="" />
+        <img src={parrot5} alt="" />
+        <img src={parrot6} alt="" />
+        <img src={parrot7} alt="" />
+    </div> );
+}
+ 
+export default Forest;
