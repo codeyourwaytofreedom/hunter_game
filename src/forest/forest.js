4b06924a3611fc63964d2836a858288417bfcf59
codeyourwaytofreedom <can.kolej@gmail.com>
2022-12-29 22:08:34 +0300

    Auto moving bird introduced.
    Will test random movements vertical and horizontal.
    
    Keep going.
    
    Remember why you started.

--- src/forest/forest.js
+++ src/forest/forest.js
@@ -15,26 +15,31 @@ const Forest = () => {
 
     const [coors, setCoors] = useState([]);
     const [hit, setHit] = useState("");
-    const [random_cor, setRandomcor] = useState([]);
+    const [random_cor, setRandomcor] = useState([0,20]);
     useEffect(() => {
         // 👇️ get global mouse coordinates
         const handleWindowMouseMove = event => {
             setCoors([event.screenX, event.screenY])
         };
-        function ranInt(max) {
-            return Math.floor(Math.random() * max);
-        }
-        setTimeout(() => {
-            setRandomcor([0, ranInt(400)])
-        }, 2000);
           
         document.addEventListener('mousemove', handleWindowMouseMove);
     
         return () => {
           document.removeEventListener('mousemove', handleWindowMouseMove);
         };
-      }, [random_cor]);
+      }, []);
 
+      useEffect(() => {
+        function ranInt(max) {
+            return Math.floor(Math.random() * max);
+        }
+        //setRandomcor([0, ranInt(600)])
+      }, []);
+    const step = 0.035;
+    const vertical = 0;
+    setTimeout(() => {
+    setRandomcor([random_cor[0]+step, random_cor[1]+vertical])
+    }, 1);
 
     return ( 
     <>
@@ -46,7 +51,7 @@ const Forest = () => {
 {/*         <div className="forest_parrot2" onClick={()=> setHit("HIT")}>
 
         </div> */}
-        <div className="forest_parrot_effect" style={{left:random_cor[0], top:random_cor[1]}}>
+        <div className="forest_parrot_effect" style={{left:`${random_cor[0]}vw`, top:`${random_cor[1]}vh`}}>
             
         </div>
 
